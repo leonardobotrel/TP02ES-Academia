e61670d7a69207734bfeb871921557a2c29d1471
leonardobotrel <leonardobotrel@gmail.com>
2020-10-20 16:36:55 -0300

    Correção da parte de usuário no menu lateral

--- Academia/resources/views/layouts/navbars/sidebar.blade.php
+++ Academia/resources/views/layouts/navbars/sidebar.blade.php
@@ -11,30 +11,12 @@
                     <p>{{ __('Dashboard') }}</p>
                 </a>
             </li>
-            <li>
-                <a data-toggle="collapse" href="#laravel-examples" aria-expanded="true">
-                    <i class="fab fa-laravel" ></i>
-                    <span class="nav-link-text" >{{ __('Laravel Examples') }}</span>
-                    <b class="caret mt-1"></b>
-                </a>
-
-                <div class="collapse show" id="laravel-examples">
-                    <ul class="nav pl-4">
-                        <li @if ($pageSlug == 'profile') class="active " @endif>
+            <li @if ($pageSlug == 'profile') class="active " @endif>
                             <a href="{{ route('profile.edit')  }}">
                                 <i class="tim-icons icon-single-02"></i>
-                                <p>{{ __('User Profile') }}</p>
+                                <p>{{ __('Usuário') }}</p>
                             </a>
                         </li>
-                        <li @if ($pageSlug == 'users') class="active " @endif>
-                            <a href="{{ route('user.index')  }}">
-                                <i class="tim-icons icon-bullet-list-67"></i>
-                                <p>{{ __('User Management') }}</p>
-                            </a>
-                        </li>
-                    </ul>
-                </div>
-            </li>
             <li @if ($pageSlug == 'icons') class="active " @endif>
                 <a href="{{ route('planos.index') }}">
                     <i class="tim-icons icon-atom"></i>
