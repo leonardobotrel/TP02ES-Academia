81232779d6ded7786398e8a85436b5c5f19941cd
leonardobotrel <leonardobotrel@gmail.com>
2020-11-04 13:42:28 -0300

    Agora o usuario precisa estar logado para acessar: matriculas turmas planos modalidades

--- Academia/routes/web.php
+++ Academia/routes/web.php
@@ -10,38 +10,36 @@ Auth::routes();
 Route::resource('profiles', 'App\Http\Controllers\Admin\ProfileController');
 Route::get('/profiles_detalhe{id}', [App\Http\Controllers\Admin\ProfileController::class, 'detail'])->name('matriculas.detail');
 
-//Rotas Para Matriculas
-Route::resource('matriculas', 'App\Http\Controllers\Admin\MatriculaController');
-Route::get('/matriculas_detalhe{id}', [App\Http\Controllers\Admin\MatriculaController::class, 'detail'])->name('matriculas.detail');
-Route::any('/matricula_pesquisa', [App\Http\Controllers\Admin\MatriculaController::class, 'search'])->name('matriculas.search');
-
-//Rotas de Turmas
-Route::post('/turmas', [App\Http\Controllers\Admin\TurmaController::class, 'salvar'])->name('turma.salvar');
-Route::get('/turmas', [App\Http\Controllers\Admin\TurmaController::class, 'index'])->name('turma.index');
-Route::get('/turmas_criar', [App\Http\Controllers\Admin\TurmaController::class, 'criar'])->name('turmas.criar');
-
-
-//Rotas de Planos
-Route::delete('/planos_detalhe{id}', [App\Http\Controllers\Admin\PlanoController::class, 'deletar'])->name('planos.deletar');
-Route::any('/pesquisa', [App\Http\Controllers\Admin\PlanoController::class, 'pesquisa'])->name('planos.pesquisa');
-Route::get('/planos_detalhe{id}', [App\Http\Controllers\Admin\PlanoController::class, 'detalhe'])->name('planos.detalhe');
-Route::post('/planos', [App\Http\Controllers\Admin\PlanoController::class, 'salvar'])->name('planos.salvar');
-Route::get('/planos', [App\Http\Controllers\Admin\PlanoController::class, 'index'])->name('planos.index');
-Route::get('/planos_criar', [App\Http\Controllers\Admin\PlanoController::class, 'criar'])->name('planos.criar');
-
-
 //Rotas da PAgina Inicial
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
 
-//Rotas para modalidades
-Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home')->middleware('auth');
-Route::get('/modalidade', [App\Http\Controllers\Admin\ModalidadeController::class, 'index'])->name('modalidade.index');
-Route::get('/modalidade_criar', [App\Http\Controllers\Admin\ModalidadeController::class, 'criar'])->name('modalidade.criar');
-Route::get('/modalidade_detalhe{id}', [App\Http\Controllers\Admin\ModalidadeController::class, 'detalhe'])->name('modalidade.detalhe');
-Route::any('/modalidade_pesquisa', [App\Http\Controllers\Admin\ModalidadeController::class, 'pesquisa'])->name('modalidade.pesquisa');
-Route::post('/modalidade', [App\Http\Controllers\Admin\ModalidadeController::class, 'salvar'])->name('modalidade.salvar');
-
 Route::group(['middleware' => 'auth'], function () {
+	//Rotas Para Matriculas
+	Route::resource('matriculas', 'App\Http\Controllers\Admin\MatriculaController');
+	Route::get('/matriculas_detalhe{id}', [App\Http\Controllers\Admin\MatriculaController::class, 'detail'])->name('matriculas.detail');
+	Route::any('/matricula_pesquisa', [App\Http\Controllers\Admin\MatriculaController::class, 'search'])->name('matriculas.search');
+
+	//Rotas de Turmas
+	Route::post('/turmas', [App\Http\Controllers\Admin\TurmaController::class, 'salvar'])->name('turma.salvar');
+	Route::get('/turmas', [App\Http\Controllers\Admin\TurmaController::class, 'index'])->name('turma.index');
+	Route::get('/turmas_criar', [App\Http\Controllers\Admin\TurmaController::class, 'criar'])->name('turmas.criar');
+
+	//Rotas de Planos
+	Route::delete('/planos_detalhe{id}', [App\Http\Controllers\Admin\PlanoController::class, 'deletar'])->name('planos.deletar');
+	Route::any('/pesquisa', [App\Http\Controllers\Admin\PlanoController::class, 'pesquisa'])->name('planos.pesquisa');
+	Route::get('/planos_detalhe{id}', [App\Http\Controllers\Admin\PlanoController::class, 'detalhe'])->name('planos.detalhe');
+	Route::post('/planos', [App\Http\Controllers\Admin\PlanoController::class, 'salvar'])->name('planos.salvar');
+	Route::get('/planos', [App\Http\Controllers\Admin\PlanoController::class, 'index'])->name('planos.index');
+	Route::get('/planos_criar', [App\Http\Controllers\Admin\PlanoController::class, 'criar'])->name('planos.criar');
+
+	//Rotas para modalidades
+	Route::get('/home', 'App\Http\Controllers\HomeController@index')->name('home')->middleware('auth');
+	Route::get('/modalidade', [App\Http\Controllers\Admin\ModalidadeController::class, 'index'])->name('modalidade.index');
+	Route::get('/modalidade_criar', [App\Http\Controllers\Admin\ModalidadeController::class, 'criar'])->name('modalidade.criar');
+	Route::get('/modalidade_detalhe{id}', [App\Http\Controllers\Admin\ModalidadeController::class, 'detalhe'])->name('modalidade.detalhe');
+	Route::any('/modalidade_pesquisa', [App\Http\Controllers\Admin\ModalidadeController::class, 'pesquisa'])->name('modalidade.pesquisa');
+	Route::post('/modalidade', [App\Http\Controllers\Admin\ModalidadeController::class, 'salvar'])->name('modalidade.salvar');
+
 	//Rotas para exame medico (Aptidao fisica)
 	Route::get('/aptidaoFisica/index', [App\Http\Controllers\Admin\AptidaoFisicaController::class, 'index'])->name('aptidao.index');
 	Route::get('/aptidaoFisica/cadastro', [App\Http\Controllers\Admin\AptidaoFisicaController::class, 'cadastro'])->name('aptidao.cadastro');
