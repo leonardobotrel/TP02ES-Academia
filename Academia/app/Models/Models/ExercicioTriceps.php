0dfcb21362d032f1ae19a992743d6b0ba61da372
Marcos Xavier <marcos_mjsx@hotmail.com>
2020-11-13 17:09:33 -0300

    Criando Models para Criar Exercicios

---
+++ Academia/app/Models/Models/ExercicioTriceps.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ExercicioTriceps extends Model
+{
+    use HasFactory;
+}
